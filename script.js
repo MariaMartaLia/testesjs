3d3b6fb1bfe3e12936edcb5cee056779d99b2a83
Maria Marta (Lia) <160326282+MariaMartaLia@users.noreply.github.com>
2024-03-13 15:09:46 -0300

    teste logica 1

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-Document.write( ${dias})
+let visor = window.document.getElementById('visor')
+let n0 = document.getElementById('valor0')
+let n1 = document.getElementById('valor1')
+let n2 = document.getElementById('valor2')
+let n3 = document.getElementById('valor3')
+let n4 = document.getElementById('valor4')
+let n5 = document.getElementById('valor5')
+let n6 = document.getElementById('valor6')
+let n7 = document.getElementById('valor7')
+let n8 = document.getElementById('valor8')
+let n9 = document.getElementById('valor9')
+let soma = document.getElementById('operador1')
+let sub = document.getElementById('operador2')
+let divi = document.getElementById('operador4')
+let mult = document.getElementById('operador3')
+let res = document.getElementById('res')
+let ponto = document.getElementById('ponto')
+let limpar =document.getElementById('limpar')
+let v =[n0,n1,n2,n3,n4,n5,n6,n7,n8,n9]
+ function clicar(){
+    if(res=v.indexOf[0] ){
+        
+        
+    }
+ }
+
